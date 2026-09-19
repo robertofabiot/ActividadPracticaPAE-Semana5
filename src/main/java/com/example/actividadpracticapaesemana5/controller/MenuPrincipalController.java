4401be95d0bf38fc70aa39264f4dfb54d70a5ab3
Jarquin23 <amjarquinc@uamv.edu.ni>
2026-09-19 16:55:50 -0600

    Desactivacion de botones de la pantalla actual

--- src/main/java/com/example/actividadpracticapaesemana5/controller/MenuPrincipalController.java
+++ src/main/java/com/example/actividadpracticapaesemana5/controller/MenuPrincipalController.java
@@ -7,16 +7,27 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.control.MenuItem;
 
 import java.util.Optional;
 
 public class MenuPrincipalController {
     @FXML private Label lblBienvenida;
     @FXML private Label lblEstado;
+    @FXML private MenuItem miIrMenu;
+    @FXML private MenuItem miIrRegistro;
+    @FXML private MenuItem miIrConsulta;
 
     @FXML
     public void initialize() {
         lblBienvenida.setText("Menú Principal");
+
+        miIrMenu.setDisable(true);
+    }
+
+    @FXML
+    public void irAlMenu(ActionEvent event) {
+        // No hace nada al ya estar en el menú principal.
     }
 
     @FXML
